fff8e6a12644d281ae4927b11f464058439811b8
horizon <liudxp@live.cn>
2019-02-20 10:53:18 +0800

    Initial commit

---
+++ src/image_segementation/ncut.py
@@ -0,0 +1,14 @@
+def ncut_graph_matrix(im, sigma_d=1e2, sigma_g=1e2):
+    """用于创建归一化割的矩阵，其中sigma_d和sigma_g像素相似性的权重参数"""
+
+    m, n = im.shape[:2]
+    N = m * n
+
+    # 归一化，并创建RGB或灰度向量
+    if len(im.shape) == 3:
+        for i in range(3):
+            im[:, :, i] = im[:, :, i].max()
+        vim = im.reshape((-1, 3)) # 自动计算行数，列数为3
+    else:
+        im /= im.max()
+        vim = im.flatten() # 折叠为一维
